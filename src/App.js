52d297e2edeca6dda1b341bf932a21304ce9d40a
kengne ruphine <ruphinekengne@gmail.com>
2020-08-22 14:29:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,241 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  InfoWindow,
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+import Geocode from "react-geocode";
+import { Descriptions } from 'antd';
+import AutoComplete from 'react-google-autocomplete'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+Geocode.setApiKey("AIzaSyCVQ7s7lymAPgLWLgK7fG6VcOPpmGLiI3g")
+
+class App extends React.Component {
+  state = {
+    address: "",
+    city: "",
+    area: "",
+    state: "",
+    zoom: 15,
+    height: 400,
+    mapPosition: {
+      lat: 0,
+      lng: 0,
+    },
+    markerPosition: {
+      lat: 0,
+      lng: 0
+    }
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          mapPosition: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          },
+          markerPosition: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+        }, () => {
+          Geocode.fromLatLng(position.coords.latitude, position.coords.longitude)
+            .then(response => {
+              const address = response.results[0].formatted_address,
+                addressArray = response.results[0].address_components,
+                city = this.getCity(addressArray),
+                area = this.getArea(addressArray),
+                state = this.getState(addressArray);
+
+              this.setState({
+                address: (address) ? address : '',
+                area: (area) ? area : '',
+                city: (city) ? city : '',
+                state: (state) ? state : '',
+              })
+            },
+            error => {
+              console.error(error);
+          })
+        })
+      })
+    }else {
+      console.error("Geolocation is not supported by this browser!");
+  }
+  };
+
+  getCity = (addressArray) => {
+    let city = '';
+
+    for (let index = 0; index < addressArray.length; index++) {
+      if (addressArray[index].types[0] && 'administrative_area_level_2' === addressArray[index].types[0]) {
+        city = addressArray[index].long_name;
+        return city;
+      }
+
+    }
+  }
+
+  getArea = (addressArray) => {
+    let area = '';
+    for (let i = 0; i < addressArray.length; i++) {
+      if (addressArray[i].types[0]) {
+        for (let j = 0; j < addressArray[i].types.length; j++) {
+          if ('sublocality_level_1' === addressArray[i].types[j] || 'locality' === addressArray[i].types[j]) {
+            area = addressArray[i].long_name;
+            return area;
+          }
+        }
+      }
+    }
+  };
+
+  getState = (addressArray) => {
+    let state = '';
+    for (let i = 0; i < addressArray.length; i++) {
+      for (let i = 0; i < addressArray.length; i++) {
+        if (addressArray[i].types[0] && 'administrative_area_level_1' === addressArray[i].types[0]) {
+          state = addressArray[i].long_name;
+          return state;
+        }
+      }
+    }
+  };
+
+  onMarkerDragEnd = (event) => {
+    //shows the latitude and longitude after the markers end
+    let newLat = event.latLng.lat();
+    let newLng = event.latLng.lng();
+
+    Geocode.fromLatLng(newLat, newLng)
+      .then(response => {
+        const address = response.results[0].formatted_address,
+          addressArray = response.results[0].address_components,
+          city = this.getCity(addressArray),
+          area = this.getArea(addressArray),
+          state = this.getState(addressArray);
+
+        this.setState({
+          address: (address) ? address : '',
+          area: (area) ? area : '',
+          city: (city) ? city : '',
+          state: (state) ? state : '',
+          markerPosition: {
+            lat: newLat,
+            lng: newLng
+          },
+          mapPosition: {
+            lat: newLat,
+            lng: newLng
+          },
+        })
+      },
+      error => {
+        console.error(error);
+    })
+  }
+
+  onPlaceSelected = (place) => {
+    console.log('plc', place);
+    const address = place.formatted_address,
+      addressArray = place.address_components,
+      city = this.getCity(addressArray),
+      area = this.getArea(addressArray),
+      state = this.getState(addressArray),
+      latValue = place.geometry.location.lat(),
+      lngValue = place.geometry.location.lng();
+
+    console.log('latvalue', latValue)
+    console.log('lngValue', lngValue)
+
+    // Set these values in the state.
+    this.setState({
+      address: (address) ? address : '',
+      area: (area) ? area : '',
+      city: (city) ? city : '',
+      state: (state) ? state : '',
+      markerPosition: {
+        lat: latValue,
+        lng: lngValue
+      },
+      mapPosition: {
+        lat: latValue,
+        lng: lngValue
+      },
+    })
+  };
+
+  onInfoWindowClose = (event) => { };
+  render() {
+    const AsyncMap = withScriptjs(
+      withGoogleMap(
+        props=>(
+  
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
+      >
+        <div className="hover-text">
+        <Marker
+          draggable={true}
+          onDragEnd={this.onMarkerDragEnd}
+            position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+              {/* <InfoWindow
+            position={{ lat: (this.state.markerPosition.lat + 0.0018), lng: this.state.markerPosition.lng }}
+          >
+            <div>
+              <span style={{ padding: 0, margin: 0 }}>{this.state.address}</span>
+            </div>
+          </InfoWindow> */}
+              </Marker>
+              <div className="drag">Drag me</div>
+              </div>
+
+        <AutoComplete
+          style={{
+            width: '100%',
+            height: '40px',
+            paddingLeft: '16px',
+            marginTop: '2px',
+            marginBottom: '2rem'
+          }}
+          onPlaceSelected={this.onPlaceSelected}
+          defaultValue={this.state.address}
+
+          types={['(regions)']}
+        />
+      </GoogleMap>
+    
+    )
+  )
+);
+
+
+    return (
+      <div style={{ padding: '1rem', margin: '0 auto', maxWidth: 1000 }}>
+        <h1>Google Map Basic</h1>
+        <Descriptions bordered>
+          <Descriptions.Item label="City">{this.state.city}</Descriptions.Item>
+          <Descriptions.Item label="Area">{this.state.area}</Descriptions.Item>
+          <Descriptions.Item label="State">{this.state.state}</Descriptions.Item>
+          <Descriptions.Item label="Address">{this.state.address}</Descriptions.Item>
+        </Descriptions>
+        <p>Select a new location by dragging the marker</p>
+        <AsyncMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVQ7s7lymAPgLWLgK7fG6VcOPpmGLiI3g&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
